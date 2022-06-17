489711d6ad111293361c112fff6c5a8dd0631544
Anirudh A V <anirudh.av02@gmail.com>
2022-06-17 20:47:25 +0530

    database schema altered

--- office_management/emp_app/models.py
+++ office_management/emp_app/models.py
@@ -18,13 +18,13 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=100, null=False)
     last_name = models.CharField(max_length=100)
     dept = models.ForeignKey(Department, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
     salary = models.IntegerField(default=0)
+    hire_date = models.DateField()
     address = models.CharField(max_length=300)
     city = models.CharField(max_length=100)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    phone = models.IntegerField(default=0)
-    hire_date = models.DateField()
+    phone = models.CharField(null=False, max_length=10)
 
 
     def __str__(self):
-        return "%s %s %s" %(self.first_name, self.last_name, self.phone)
+        return "%s %s %s" %(self.first_name, self.last_name, self.role)
