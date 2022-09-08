615a422f612f756395456db5aa1da67b68b438c1
Anirudh A V <anirudh.av02@gmail.com>
2022-09-08 20:51:52 +0530

    Project name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'office_emp_proj.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'office_management.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- office_emp_proj/asgi.py
+++ office_emp_proj/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'office_emp_proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'office_management.settings')
 
 application = get_asgi_application()
